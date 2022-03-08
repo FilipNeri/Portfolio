7191dd4744c4f155e4585e6f22873f684b8a2435
Filipe Neri <filipe.neri50@gmail.com>
2022-03-07 23:28:07 -0300

    Nome topo

--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -1,30 +1,32 @@
-import "./topbar.scss"
-import {Person, Mail} from "@material-ui/icons"
+import "./topbar.scss";
+import { Person, Mail } from "@material-ui/icons";
 
-export default function Topbar({menuOpen, setMenuOpen}){
-    return(
-        <div className={"topbar " + (menuOpen && "active")}>
-            <div className='wrapper'>
-            <div className="left">
-                <a href="#intro" className='logo'>Filipe Neri</a>
-                <div className="itemContainer">
-                    <Person className="icon"/>
-                    <span>+55 48 98503-1830</span>
-                </div>
-                <div className="itemContainer">
-                    <Mail className="icon"/>
-                    <span>filipe.neri50@gmail.com</span>
-                </div>
-            </div>
-
-            <div className="right">
-                <div className="hamburguer" onClick={()=>setMenuOpen(!menuOpen)}>
-                    <span className="line1"></span>
-                    <span className="line2"></span>
-                    <span className="line3"></span>
-                </div>
-            </div>
-            </div>
+export default function Topbar({ menuOpen, setMenuOpen }) {
+  return (
+    <div className={"topbar " + (menuOpen && "active")}>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            Filipe Neri
+          </a>
+          <div className="itemContainer">
+            <Person className="icon" />
+            <span>+55 48 98503-1830</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>filipe.neri50@gmail.com</span>
+          </div>
         </div>
-    )
+
+        <div className="right">
+          <div className="hamburguer" onClick={() => setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
